fc9a21f2449814073015abd52ad82e56980ef820
Himanshi <147384570+himanshi-0908@users.noreply.github.com>
2025-08-16 10:52:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,53 +1,21 @@
-from flask import Flask,request,render_template
-import numpy as np
-import pandas
-import sklearn
+import streamlit as st
 import pickle
 
-# importing model
-model = pickle.load(open('model.pkl','rb'))
-sc = pickle.load(open('standscaler.pkl','rb'))
-ms = pickle.load(open('minmaxscaler.pkl','rb'))
+# Load model
+model = pickle.load(open("model.pkl", "rb"))
 
-# creating flask app
-app = Flask(__name__)
+st.title("Crop Recommendation System")
 
-@app.route('/')
-def index():
-    return render_template("index.html")
+# Input fields
+N = st.number_input("Nitrogen", min_value=0)
+P = st.number_input("Phosphorus", min_value=0)
+K = st.number_input("Potassium", min_value=0)
+temperature = st.number_input("Temperature")
+humidity = st.number_input("Humidity")
+ph = st.number_input("pH")
+rainfall = st.number_input("Rainfall")
 
-@app.route("/predict",methods=['POST'])
-def predict():
-    N = request.form['Nitrogen']
-    P = request.form['Phosporus']
-    K = request.form['Potassium']
-    temp = request.form['Temperature']
-    humidity = request.form['Humidity']
-    ph = request.form['Ph']
-    rainfall = request.form['Rainfall']
-
-    feature_list = [N, P, K, temp, humidity, ph, rainfall]
-    single_pred = np.array(feature_list).reshape(1, -1)
-
-    scaled_features = ms.transform(single_pred)
-    final_features = sc.transform(scaled_features)
-    prediction = model.predict(final_features)
-
-    crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
-                 8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
-                 14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
-                 19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
-
-    if prediction[0] in crop_dict:
-        crop = crop_dict[prediction[0]]
-        result = "{} is the best crop to be cultivated right there".format(crop)
-    else:
-        result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
-    return render_template('index.html',result = result)
-
-
-
-
-# python main
-if __name__ == "__main__":
-    app.run(debug=True)
+if st.button("Predict Crop"):
+    data = [[N, P, K, temperature, humidity, ph, rainfall]]
+    prediction = model.predict(data)[0]
+    st.success(f"Recommended Crop: {prediction}")
